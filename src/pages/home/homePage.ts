51b0a1ead64b758326c8d62eaef85d980d467bbf
kirzyka <kirzyka@gmail.com>
2025-12-29 18:34:42 +0200

    homePage

--- src/pages/home/homePage.ts
+++ src/pages/home/homePage.ts
@@ -10,15 +10,19 @@ export function homePage(): HTMLElement {
     const ctx: IAppModel = useContext<IAppModel>(AppContext);
     const getLabel = () => `Число книг: ${ctx.books.get().length}`;
     const label: HTMLElement = span().innerHTML(getLabel()).get();
-    const link: HTMLAnchorElement = a().title("About").innerHTML("About").href("#/about").get();
-    const myButton: HTMLButtonElement = button()
-        .innerHTML("Добавить книгу")
-        .onClick(() => ctx.books.set([...ctx.books.get(), { title: "Война и мир" }]))
-        .get();
-
     ctx.books.subscribe(() => {
         label.innerHTML = getLabel();
     });
 
-    return div().children([label, link, myButton]).className("page main-page _flex _flex-row _flex-gap_10").get();
+    return div()
+        .children([
+            label,
+            a().title("About").innerHTML("About").href("#/about").get(),
+            button()
+                .innerHTML("Добавить книгу")
+                .onClick(() => ctx.books.set([...ctx.books.get(), { title: "Война и мир" }]))
+                .get(),
+        ])
+        .className("page main-page _flex _flex-row _flex-gap_10")
+        .get();
 }
